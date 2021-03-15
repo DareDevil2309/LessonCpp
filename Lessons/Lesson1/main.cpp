3899212f4a7d8bdd5a65dc101790c9d18bee388e
Albiflora9 <yukiningyou@gmail.com>
2021-03-15 22:28:31 +0500

    Help me bro.:D

--- Lessons/Lesson1/main.cpp
+++ Lessons/Lesson1/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <ctype.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world! Huck you" << endl;
-
-    return 0;
+int letter;
+cout << "Введите букву: "<< endl;
+cin >> letter;
+cout << islower(letter) << endl;
+return 0;
 }
