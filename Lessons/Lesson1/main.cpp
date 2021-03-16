60926b7be8e98606d26fabd30f88bc7fb2fdc669
Vladislav <daredevil2309@gmail.com>
2021-03-16 23:57:19 +0500

    Поменял тип переменной на char

--- Lessons/Lesson1/main.cpp
+++ Lessons/Lesson1/main.cpp
@@ -1,15 +1,14 @@
 #include <iostream>
-#include <iomanip>
-#include <cmath>
 #include <ctype.h>
 
 using namespace std;
 
 int main()
 {
-int letter;
-cout << "Введите букву: "<< endl;
-cin >> letter;
-cout << islower(letter) << endl;
-return 0;
+	setlocale(0, "");
+	char letter;
+	cout << "Введите букву: "<< endl;
+	cin >> letter;
+	cout << islower(letter) << endl;
+	return 0;
 }
