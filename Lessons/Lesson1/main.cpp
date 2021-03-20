ff9376b5cfb887c31480f9d227b5696cab4914b7
Albiflora9 <yukiningyou@gmail.com>
2021-03-20 23:46:07 +0500

    I hate british money

--- Lessons/Lesson1/main.cpp
+++ Lessons/Lesson1/main.cpp
@@ -7,9 +7,13 @@ using namespace std;
 
 int main()
 {
-int letter;
-cout << "Введите букву: "<< endl;
-cin >> letter;
-cout << islower(letter) << endl;
+float pound, shil, penny;
+cout << "Введите количество фунтов: " << endl;
+cin >> pound;
+cout << "Введите количество шиллингов: " << endl;
+cin >> shil;
+cout << "Введите количество пенсов: " << endl;
+cin >> penny;
+cout << "Десятичных фунтов: " << "J" << (penny + shil*12 + pound*20*12)/100 << endl;
 return 0;
 }
